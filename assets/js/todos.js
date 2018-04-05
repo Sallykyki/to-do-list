120d2605217609aa78e89308ab0052e0f916acb0
Sally <huong.pq12@gmail.com>
2018-04-05 16:54:40 +0300

    add style

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,31 @@
 //Check off specific todos by clicking
-$("li").click(function() {
+// $("li").on("click", function() {
+$("ul").on("click", "li", function() {
+  //Click() only adds listeners for existing elements
+  //on() will aadd listeners for  all potential future elements
+  //only add the listener on elements that exist when this code runs the first time
   $(this).toggleClass("completed");
 });
 
 //Click on X to delete Todo
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
   $(this)
     .parent()
     .fadeOut(500, function() {
       $(this).remove();
     });
   event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    //13 = enter
+    //grabbing new todo text from input
+    var todoText = $(this).val();
+    $(this).val("");
+    //creat a new li
+    $("ul").append(
+      "<li><span><i class='fas fa-trash-alt'></i> </span>" + todoText + "</li>"
+    );
+  }
 });
